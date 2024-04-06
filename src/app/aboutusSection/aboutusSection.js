c9c9639fb0ac5dbcfb4f19eb71bbd43810246e32
Mukesh Jat <mukeshjat3355@gmail.com>
2024-04-06 17:57:29 +0530

    update ui and content

--- src/app/aboutusSection/aboutusSection.js
+++ src/app/aboutusSection/aboutusSection.js
@@ -14,8 +14,8 @@ const Aboutus = () => {
                         <div className="col-lg-6 d-flex align-items-center">
                             <div className="content-wrapper">
                                 <h2>About us</h2>
-                                <p>At KingsClub, we love games that bring people together. Our app lets you play Ludo with friends and win real money! We've worked hard to make it fair and fun for everyone. Join us at KingsClub and enjoy gaming that rewards you!</p>
-                                <p> we believe in the power of play. Our mission is to make gaming not just entertaining but rewarding too. With our app, you can dive into thrilling Ludo challenges, compete with friends, and win exciting cash prizes. We are committed to creating a platform that is fair, secure, and accessible to everyone. Whether you're a casual player or a seasoned gamer, KingsClub welcomes you to a world of fun and rewards. Join us today and let's play, compete, and win together!</p>
+                                <p>At KingsClub, we love games that bring people together. Our app lets you play Ludo with friends and win real money! We &lsquo; ve worked hard to make it fair and fun for everyone. Join us at KingsClub and enjoy gaming that rewards you!</p>
+                                <p> we believe in the power of play. Our mission is to make gaming not just entertaining but rewarding too. With our app, you can dive into thrilling Ludo challenges, compete with friends, and win exciting cash prizes. We are committed to creating a platform that is fair, secure, and accessible to everyone. Whether you are a casual player or a seasoned gamer, KingsClub welcomes you to a world of fun and rewards. Join us today and let &lsquo;s play, compete, and win together!</p>
                             </div>
                         </div>
                     </div>
