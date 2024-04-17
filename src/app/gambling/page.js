2e9f7a8f42bd33b295dfb8ae5fd2612a0ec5502e
Mukesh Jat <mukeshjat3355@gmail.com>
2024-04-17 15:20:12 +0530

    comprass images and add font

--- src/app/gambling/page.js
+++ src/app/gambling/page.js
@@ -3,81 +3,11 @@
 import React, { useEffect, useState } from "react";
 import Header from "../CommenSection/Header";
 import Footer from "../CommenSection/footer";
-import { getCancationContent } from "../service/apicalls";
+import { getCancationContent, getRefundDataContent } from "../service/apicalls";
 
 export default function Page() {
     const [cancelData, setCancelData] = useState({})
-    const cancelationData = {
-        "cancellationPolicy": {
-            "title": "Cancellation Policy",
-            "content": [
-                "For cancellations, please contact us via WhatsApp or email: help.kingsclub@gmail.com",
-                "Requests received later than 2 business days before the scheduled match will be considered cancellations for the next match."
-            ]
-        },
-        "themePlayerRefunds": {
-            "title": "Theme Player or Fresh ID Player Refunds",
-            "content": [
-                "If you join a match as a Theme player or play with a Theme/Fresh ID player, our refund policy applies as follows:",
-                "Cancellation within 10% of Gameplay: If you cancel the game after playing less than 10% of the total duration, you will receive a 50% refund (entry fee).",
-                "Cancellation after 10% of Gameplay: If you cancel after playing more than 10% of the duration, the losing player will forfeit the entry fee, and the winning player will receive only 50% of the total win amount."
-            ]
-        },
-        "fairPlaySportsmanship": {
-            "title": "Fair Play and Sportsmanship",
-            "content": [
-                "Foul Language and Slang: Using any kind of foul language or slang towards your opponent without a valid reason will result in game cancellation and a potential fine (no refunds in such cases).",
-                "Slow Play: Playing excessively slow without justification will lead to a game loss. If you have a valid reason for slow play (requires verification), the game may be canceled after 30 minutes from the start."
-            ]
-        },
-        "inappropriateContent": {
-            "title": "Inappropriate Content",
-            "content": [
-                "Uploading a profile picture with nudity is strictly prohibited. Doing so will result in a permanent account suspension."
-            ]
-        },
-        "tournamentTimeLimits": {
-            "title": "Tournament Time Limits",
-            "content": [
-                "In tournament matches, exceeding the 15-minute time limit may lead to match cancellation without a refund."
-            ]
-        }
-    }
-    const refundData = {
-        "RefundPolicy": {
-            "title": "Refund Policy",
-            "content": [
-                "KingsClub strives for customer satisfaction. We may offer refunds on a case-by-case basis for genuine reasons verified through investigation. Here's an overview of our refund policy."
-            ]
-        },
-        "GeneralPolicy": {
-            "title": "General Policy",
-            "content": [
-                "Refunds are not guaranteed and will be evaluated based on the specific situation."
-            ]
-        },
-        "ScenariosforRefunds:": {
-            "title": "Scenarios for Refunds:",
-            "content": [
-                "Cancellation within Allowed Window (see Cancellation Policy): Refer to the Cancellation Policy section for details on eligible cancellation windows and partial refunds based on gameplay progress (Theme/Fresh ID players).",
-                "Technical Issues: If technical issues prevent you from playing the game.",
-                "Rule Violations by Opponent (with Proof): If your opponent violates game rules and you can provide valid proof."
-
-            ]
-        },
-        "Non-RefundableSituations": {
-            "title": "Non-Refundable Situations",
-            "content": [
-                "Delays in uploading results during zero commission hours (if applicable), using foul language, playing excessively slow without justification, uploading inappropriate profile pictures, or exceeding tournament time limits may lead to match cancellation and no refunds will be issued in these cases."
-            ]
-        },
-        "LateorMissingRefunds": {
-            "title": "Late or Missing Refunds",
-            "content": [
-                "If you haven't received a refund after a reasonable timeframe (usually determined by your bank's processing time), please check your bank statement first. If the issue persists, contact us at help.kingsclub@gmail.com for further assistance"
-            ]
-        },
-    }
+    const [refundData, setRefundData] = useState({})
 
     const getContent = async (e) => {
         await getCancationContent()
@@ -90,8 +20,20 @@ export default function Page() {
                 }
             })
     }
+    const getContent2 = async (e) => {
+        await getRefundDataContent()
+            .then((res) => {
+                if (res.success) {
+                    console.log("res", res)
+                    setCancelData(res?.data)
+                } else {
+                    console.log("err", res)
+                }
+            })
+    }
     useEffect(() => {
         getContent()
+        getContent2()
     }, [])
 
 
--- src/app/layout.js
+++ src/app/layout.js
@@ -16,6 +16,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;300;400;500;600;700;800;900&display=swap" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Playfair+Display:wght@400;500;600;700;800;900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"></link>
         <script src="https://kit.fontawesome.com/ffd603c3cc.js" crossOrigin="anonymous" async ></script>
         <link rel="icon" href="./favicon.ico" sizes="any" />
       </head>
